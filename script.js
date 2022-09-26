a39a682fe4c3b5e85dcf56756123e2e16432f588
Bunász István <71004402+bunaszistvan@users.noreply.github.com>
2022-09-26 14:44:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+(() => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            let photos = JSON.parse(xhr.response);
+            let gallery = document.getElementById("gallery");
+            photos.map((photo) => {
+                let img = document.createElement('img');
+                img.src = photo.url;
+                img.alt = photo.title;
+                gallery.appendChild(img);
+            });
+
+        };
+    };
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos');
+    xhr.send();
+
+})();
